22ec7672e9a488dc44864e0f9e96c01287184085
AneesahK <95223746+AneesahK@users.noreply.github.com>
2024-02-23 19:44:36 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from contextlib import asynccontextmanager
 import os
 import datetime
 import json
+import random
 
 from .database import SessionLocal, engine
 from . import models
@@ -44,7 +45,7 @@ async def initialize_dbAudio(app: FastAPI):
             stringS = sound.replace(".WAV", "").split("_")
             offsetTime = int(stringS[2]) - 10
             timeStampS = datetime.datetime(int(stringS[0][:4]), int(stringS[0][4:6]), int(stringS[0][6:]), int(stringS[1][:2]), int(stringS[1][2:4]), int(stringS[1][4:]))
-            timeStampS = timeStampS + datetime.timedelta(seconds=offsetTime)
+            timeStampS = timeStampS + datetime.timedelta(seconds=offsetTime)    #Change this if we change to minute files
             locationS = stringS[3]
 
             if db.query(models.audioFile).filter_by(uri="/audio/{}".format(sound)).count() == 0:
@@ -55,14 +56,12 @@ async def initialize_dbAudio(app: FastAPI):
     db.close()
     os.chdir(current_path)
 
-async def initialize_dbStory(app: FastAPI):
-    ### Count is first number, category is the second number
+async def initialize_dbStory(app: FastAPI): 
     db = SessionLocal()
     num_sound = db.query(models.storyFile).count()
 
     with open('category.json') as f:
         d = json.load(f)
-    # print(len(d))
 
 
     current_path = os.getcwd()
@@ -75,10 +74,18 @@ async def initialize_dbStory(app: FastAPI):
             stringS = p.split(".")[0]           ##names with no dots
             c = d[stringS.split("_")[-1]]
 
-            if db.query(models.storyFile).filter_by(uri="/story/{}".format(p)).count() == 0:
-                completeJ = {"uri":"/story/{}".format(p), "count":int(stringS.split("_")[-2]), "category":c}
+            if p.split(".")[1] == "txt":
+                typeURI = "textUri"
+            else:
+                typeURI = "imageUri"    #column name depending on type of file
+
+            if db.query(models.storyFile).filter_by(count=int(stringS.split("_")[-2]),category=c).count() == 0: #none from this prize lvl
+                completeJ = {typeURI:"/story/{}".format(p), "count":int(stringS.split("_")[-2]), "category":c}
                 db.add(models.storyFile(**completeJ))
                 db.commit()
+            else:   #already exists
+                db.query(models.storyFile).filter_by(count=int(stringS.split("_")[-2]),category=c).update({typeURI:"/story/{}".format(p)})
+                
 
     db.close()
     os.chdir(current_path)
@@ -94,7 +101,7 @@ app = FastAPI(lifespan=lifespan)
 app.mount("/audio", StaticFiles(directory=("static/tenSecChunks")), name="audio")
 app.mount("/story", StaticFiles(directory=("static/stories")), name="story")
 
-@app.get("/getAudioFiles")
+@app.get("/getAudioFiles")      
 def getAudioFiles(
     startTime: int,     # 110207 -> 11.02 am 7 seconds
     duration: int,      # 60 -> 60 mins = 1hr
@@ -106,9 +113,13 @@ def getAudioFiles(
     end = start + datetime.timedelta(minutes=duration)  #adding minutes
     endString = end.strftime('%Y-%m-%d %H%M%S')
     endString = endString.split(" ")[1]
+    if start.date()!=end.date():
+        differentDays = True    #goes across 2 days, 11pm 3hrs
+    else:
+        differentDays = False   #single day, 10am 1 hr
 
-    query = db.query(
-        models.audioFile.uri,models.audioFile.timeStamp
+    query = db.query(           #getting all the timestamps for the timeframe
+        func.date(models.audioFile.timeStamp)
         ).filter(
             func.extract('hour', models.audioFile.timeStamp) * 3600 +
             func.extract('minute', models.audioFile.timeStamp) * 60 +
@@ -117,19 +128,77 @@ def getAudioFiles(
             func.extract('minute', models.audioFile.timeStamp) * 60 +
             func.extract('second', models.audioFile.timeStamp) <= int(str(endString)[0:2]) *3600 + int(str(endString)[2:4]) * 60  + int(str(endString)[4:]),
             models.audioFile.location == locationG
-            ).all()
-    
-    result = {}
-    # print(query)
-    for q in query:
-        # print(q[1].date())
-        dateOF = q[1].date()
-        if dateOF in result:
-            result[dateOF].append(q[0])
-        else:
-            result[dateOF] = [q[0]]
+            ).distinct().order_by(func.date(models.audioFile.timeStamp)).all()
 
-    return result       #dict of list
+    if len(query)==0:   #Alison's random feature
+        random_entry = db.query(models.audioFile).order_by(func.random()).first()
+        if not random_entry:
+            return "no entries in audio DB"
+        timeString = int(random_entry.timeStamp.strftime("%H%M%S"))
+        return getAudioFiles(startTime=timeString, duration=duration, locationG=locationG)
+        
+    
+    if differentDays and len(query)>1:
+        query.pop() #getting rid of largest from randomiser, so can't have 11.59pm on the last day, won't cause only 1 min of audio
+    
+    chosenDate = str(random.choice(query))
+    chosenDate = chosenDate.replace("(","").replace("'","").replace(",","").replace(")","")
+    chosenDate = chosenDate.split("-")
+    targetDate = datetime.date(int(chosenDate[0]), int(chosenDate[1]), int(chosenDate[2]))
+
+    if differentDays:       # cross over night 11.59pm to 1am next day
+        secondDay = targetDate + datetime.timedelta(days=1)
+        ##### From time to midnight
+        query = db.query(           #after start time, before end time
+        models.audioFile.uri,models.audioFile.timeStamp
+        ).filter(
+            func.date(models.audioFile.timeStamp) == targetDate,
+            func.extract('hour', models.audioFile.timeStamp) * 3600 +
+            func.extract('minute', models.audioFile.timeStamp) * 60 +
+            func.extract('second', models.audioFile.timeStamp) >= int(str(startTime)[0:2]) *3600 + int(str(startTime)[2:4]) * 60  + int(str(startTime)[4:]), 
+            func.extract('hour', models.audioFile.timeStamp) * 3600 +
+            func.extract('minute', models.audioFile.timeStamp) * 60 +
+            func.extract('second', models.audioFile.timeStamp) <= 24 *3600,          # 24 hr clock, midnight
+            models.audioFile.location == locationG
+            ).all()
+        
+        query.append(db.query(           #after start time, before end time
+        models.audioFile.uri,models.audioFile.timeStamp
+        ).filter(
+            func.date(models.audioFile.timeStamp) == secondDay,
+            func.extract('hour', models.audioFile.timeStamp) * 3600 +
+            func.extract('minute', models.audioFile.timeStamp) * 60 +
+            func.extract('second', models.audioFile.timeStamp) >= 0,        #midnight, 0 seconds
+            func.extract('hour', models.audioFile.timeStamp) * 3600 +
+            func.extract('minute', models.audioFile.timeStamp) * 60 +
+            func.extract('second', models.audioFile.timeStamp) <= int(str(endString)[0:2]) *3600 + int(str(endString)[2:4]) * 60  + int(str(endString)[4:]),
+            models.audioFile.location == locationG
+            ).all())
+        ##### From midnight to time
+    else:
+        ## doesn't cross days!
+        query = db.query(           #after start time, before end time
+        models.audioFile.uri,models.audioFile.timeStamp
+        ).filter(
+            func.date(models.audioFile.timeStamp) == targetDate,
+            func.extract('hour', models.audioFile.timeStamp) * 3600 +
+            func.extract('minute', models.audioFile.timeStamp) * 60 +
+            func.extract('second', models.audioFile.timeStamp) >= int(str(startTime)[0:2]) *3600 + int(str(startTime)[2:4]) * 60  + int(str(startTime)[4:]), 
+            func.extract('hour', models.audioFile.timeStamp) * 3600 +
+            func.extract('minute', models.audioFile.timeStamp) * 60 +
+            func.extract('second', models.audioFile.timeStamp) <= int(str(endString)[0:2]) *3600 + int(str(endString)[2:4]) * 60  + int(str(endString)[4:]),
+            models.audioFile.location == locationG
+            ).all()
+        
+    if len(query)==0:
+        return "empty query"
+
+    result = {"audioChunks":[], "date":targetDate.strftime("%Y-%m-%d")}
+    for q in query:
+        result["audioChunks"].append(q[0]) # add uri to audioChunks
+    result = json.dumps(result)
+    # print(result)
+    return result
 
 @app.get("/getAllStory")    #gives whole branch
 def home(
@@ -142,27 +211,24 @@ def home(
         categoryWord = d[str(cate)]
     
     query = db.query(
-        models.storyFile.uri, models.storyFile.count
+        models.storyFile
         ).filter(
             models.storyFile.category==categoryWord,
             models.storyFile.count<=prizeCount
             ).all()
     
-    # print(query)
-    result = {}
-    for item in query:
-        uri = item[0]
-        prize = item[1]
-        if prize in result:
-            result[prize].append(uri)
-        else:
-            result[prize] = [uri]
+    if len(query)==0:
+        return "It is empty"
     
-    # print(result)
+    result = {"stories":[]}
+    for obj in query:
+        pass
+        a = {"count": obj.count, "imageUri":obj.imageUri, "textUri":obj.textUri}
+        result["stories"].append(a)
     
-    return result       #returns a dict with key as prize count, and value is list of uris associated
+    return json.dumps(result)       #returns a dict with key as prize count, and value is list of uris associated
 
-@app.get("/getAStory")    #gives whole branch
+@app.get("/getAStory")    #gives whole branch, #CHANGE THIS SOPHIE
 def home(
     cate: int,          #category of the 4 in json
     prizeCount: int,     #Counter for the prize count they are on
@@ -170,25 +236,29 @@ def home(
 ):
     with open('category.json') as f:
         d = json.load(f)
-        categoryWord = d[str(cate)]
+        if str(cate) in d:
+            categoryWord = d[str(cate)]
+        else:
+            return "Category not valid"
     
     query = db.query(
-        models.storyFile.uri, models.storyFile.count
+        models.storyFile
         ).filter(
             models.storyFile.category==categoryWord,
             models.storyFile.count==prizeCount
             ).all()
     
-    result = [q[0] for q in query]
-    # print(result)
+    if len(query)==0:
+        return "It is empty"
+    elif len(query)>1:
+        return "multiple entries in the database for a given category and count, check database"
+    obj = query[0]
+    result = {"count": obj.count, "imageUri":obj.imageUri, "textUri":obj.textUri}
     
-    return result       #list
+    return json.dumps(result)     #list
 
 @app.get("/")
 def home(
     db: Session = Depends(get_db)   #access to db session
 ):
-    # f = db.query(models.audioFile).all()
-    # print(f)
-    # print("hi")
     return {"Success": "connected"}
